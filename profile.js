0acff0f0e28e6401954894fecf1e9dd6a9151628
ShaneEversole <eversoleshane7@gmail.com>
2024-10-21 11:23:15 -0400

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,12 @@
+const setUp = () => {
+
+    $("#name").text(
+      "Shane Eversole"
+    );
+    $("#major").text("Computer Information and Technology");
+    $("#year").text("2025");
+      $('h4').html("<em>USA</em>");
+  
+  };
+  $(document).ready(setUp);
+  
